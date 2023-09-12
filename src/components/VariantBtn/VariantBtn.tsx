77ebeaa2cec6b907361bc3cb2325eeb37f18436c
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-09-12 14:01:01 +0300

    change project design update icons

--- src/components/VariantBtn/VariantBtn.tsx
+++ src/components/VariantBtn/VariantBtn.tsx
@@ -2,39 +2,30 @@ import React from "react";
 
 import s from "./VariantBtn.module.css";
 
-import PlusCircle from "../../assets/icons/plus-circle.svg";
-import PlusSquare from "../../assets/icons/plus-square.svg";
-import Trash from "../../assets/icons/trash.svg";
-
 import { IButton } from "../../types/IButton.types";
 
 export const VariantBtn: React.FC<IButton> = ({ variant }): JSX.Element => {
-  let icon: string;
-  let buttonText: string;
+	let buttonText: string;
 
-  switch (variant) {
-    case "add-list":
-      icon = PlusSquare;
-      buttonText = "Add List";
-      break;
-    case "add-task":
-      icon = PlusCircle;
-      buttonText = "Add Task";
-      break;
-    case "delete-list":
-      icon = Trash;
-      buttonText = "Delete List";
-      break;
-    default:
-      icon = PlusCircle;
-      buttonText = "Default-btn";
-      break;
-  }
+	switch (variant) {
+		case "add-list":
+			buttonText = "Add List";
+			break;
+		case "add-task":
+			buttonText = "Add Task";
+			break;
+		case "delete-list":
+			buttonText = "Delete List";
+			break;
+		default:
+			buttonText = "Default-btn";
+			break;
+	}
 
-  return (
-    <button className={`${s.variantBtn}`}>
-      <img src={icon} alt={buttonText} />
-      <span>{buttonText}</span>
-    </button>
-  );
+	return (
+		<button className={`${s.variantBtn}`}>
+			<img src={""} alt={buttonText} />
+			<span>{buttonText}</span>
+		</button>
+	);
 };
