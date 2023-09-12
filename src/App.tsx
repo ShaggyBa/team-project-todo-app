3cd9350bd35035b82e1301ad599f124308d7d7bc
UniverseArc <93486544+UniverseArc@users.noreply.github.com>
2023-09-12 22:58:21 +0300

    added css colors and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./assets/logo.svg";
 import "./reset.css";
 import "./UI-colors.css";
-import { VariantBtn } from "./components/VariantBtn/VariantBtn";
 
 function App() {
 	return (
