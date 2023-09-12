0c1df43e4c48737256d45a076d9eb74a4e5062f8
UniverseArc <93486544+UniverseArc@users.noreply.github.com>
2023-09-12 22:53:52 +0300

    fuck git

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./assets/logo.svg";
 import "./reset.css";
-import "./UI-styles.css";
+import "./UI-colors.css";
 import { VariantBtn } from "./components/VariantBtn/VariantBtn";
 
 function App() {
@@ -10,10 +10,6 @@ function App() {
 			<header className="app-header">
 			</header>
 			<div className="container">
-				<VariantBtn variant="add-task" />
-				<VariantBtn variant="add-list" />
-				<VariantBtn variant="delete-list" />
-				<VariantBtn />
 			</div>
 		</div>
 	);
